b1259a21e4cf79455304d691c6c2e450d59782e9
hod <hody92@icloud.com>
2018-04-11 17:15:27 +0300

    also test

--- main.js
+++ main.js
@@ -3,10 +3,23 @@ var reservations = {
   'Ted': { claimed: true }
 }
 
-var name = prompt('Please enter the name for your reservation');
-
 var claimReservation = function () {
-  // write your code here!
+  //document.getElementById(reservation).style.visibility = "visible";
+  var getName = document.getElementById("isReserved").value;
+  var name = getName[0].toUpperCase() + getName.slice(1).toLowerCase();
+
+  if(reservations[name]) {
+    if(reservations[name]["claimed"]) {
+      document.getElementById("newReservation").innerHTML = name + ', your reservation is already claimed';
+    } else {
+      document.getElementById("newReservation").innerHTML = 'Welcome ' + name;
+    }
+  } else {
+    reservations[name] = { claimed: true };
+    document.getElementById("newReservation").innerHTML = name + ' you just got a new reservation';
+  }
 }
 
-claimReservation();
+
+
+ 
